0ae896d14b3c96f56bb31a89858799f955b28b57
youliso <youliso@hotmail.com>
2021-01-14 13:23:25 +0800

    更新歌单api

--- src/core/musicapi/util.ts
+++ src/core/musicapi/util.ts
@@ -29,7 +29,7 @@ export function randomUserAgent() {
     return userAgentList[num]
 }
 
-export function lyric_decode(str, needTranslate = false) {
+export function lyric_decode(str: string, needTranslate = false) {
     if (!str) {
         return needTranslate ? {
             lyric: [],
@@ -37,8 +37,8 @@ export function lyric_decode(str, needTranslate = false) {
         } : []
     }
     let list = str.replace(/\<\d+\>/g, '').split('\n')
-    const lyric_arr = []
-    let translate_lyric_arr = []
+    const lyric_arr: {}[] = []
+    let translate_lyric_arr: {}[] = []
     list.forEach((item, index) => {
         const matchs = item.match(/((\[\d+:\d+\.\d+\])+)(.*)/)
         if (matchs && matchs[1]) {
@@ -70,7 +70,7 @@ export function lyric_decode(str, needTranslate = false) {
             })
         }
     })
-    if (needTranslate && translate_lyric_arr.filter(item => item[1]).length === 0) {
+    if (needTranslate && translate_lyric_arr.filter((item: any) => item[1]).length === 0) {
         translate_lyric_arr = []
     }
     return needTranslate ? {
@@ -81,28 +81,28 @@ export function lyric_decode(str, needTranslate = false) {
 
 export const noSongsDetailMsg = '无法获取信息，请检查songId'
 
-function randomString(pattern, length) {
+function randomString(pattern: string, length: number) {
     return Array.apply(null, {length: length}).map(() => (pattern[Math.floor(Math.random() * pattern.length)])).join('')
 }
 
-export function completeCookie(cookie) {
+export function completeCookie(cookie?: string) {
     let origin = (cookie || '').split(/;\s*/).map(element => (element.split('=')[0])), extra = []
     let now = (new Date).getTime()
 
     if (!origin.includes('JSESSIONID-WYYY')) {
         let expire = new Date(now + 1800000) //30 minutes
         let jessionid = randomString('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ\\/+', 176) + ':' + expire.getTime()
-        extra.push(['JSESSIONID-WYYY=' + jessionid, 'Expires=' + expire.toGMTString()])
+        extra.push(['JSESSIONID-WYYY=' + jessionid, 'Expires=' + expire.toUTCString()])
     }
     if (!origin.includes('_iuqxldmzr_')) {
         let expire = new Date(now + 157680000000) //5 years
-        extra.push(['_iuqxldmzr_=32', 'Expires=' + expire.toGMTString()])
+        extra.push(['_iuqxldmzr_=32', 'Expires=' + expire.toUTCString()])
     }
     if ((!origin.includes('_ntes_nnid')) || (!origin.includes('_ntes_nuid'))) {
         let expire = new Date(now + 3153600000000) //100 years
         let nnid = randomString('0123456789abcdefghijklmnopqrstuvwxyz', 32) + ',' + now
-        extra.push(['_ntes_nnid=' + nnid, 'Expires=' + expire.toGMTString()])
-        extra.push(['_ntes_nuid=' + nnid.slice(0, 32), 'Expires=' + expire.toGMTString()])
+        extra.push(['_ntes_nnid=' + nnid, 'Expires=' + expire.toUTCString()])
+        extra.push(['_ntes_nuid=' + nnid.slice(0, 32), 'Expires=' + expire.toUTCString()])
     }
 
     return extra.map(x => x[0]).join('; ')
