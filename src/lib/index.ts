cb55bc221e2e9dafa8786100faa56f51eea360a8
youliso <youliso@hotmail.com>
2021-02-04 17:58:58 +0800

    Knuth-Durstenfeld shuffle

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -148,3 +148,16 @@ export function throttle(fun: Function, delay: number) {
 export function random(start: number = 0, end: number = 1) {
     return Math.floor(Math.random() * (end - start + 1) + start);
 }
+
+/**
+ * Knuth-Durstenfeld shuffle
+ * @param arr
+ */
+function shuffle(arr: string[]) {
+    let n = arr.length, random;
+    while (0 != n) {
+        random = (Math.random() * n--) >>> 0; // 无符号右移位运算符向下取整
+        [arr[n], arr[random]] = [arr[random], arr[n]] // ES6的结构赋值实现变量互换
+    }
+    return arr;
+}
