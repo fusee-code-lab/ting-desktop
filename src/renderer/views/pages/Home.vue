324d563c1deb2a10feb1e4074f8e1ac64be19a04
jctaoo <jctaoo@outlook.com>
2021-03-25 02:05:16 +0800

    reformat Home.vue

--- src/renderer/views/pages/Home.vue
+++ src/renderer/views/pages/Home.vue
@@ -1,43 +1,38 @@
 <template>
-    <div class="container" :class="platform" :style="{ '--accentColor': '#' + accentColor }">
-        <Head v-show="audioData.type === 'normal'" />
-        <div class="info" v-show="audioData.type === 'normal'">
-            <div class="left">
-                <Search></Search>
-                <Sheet></Sheet>
-                <div class="setting-but bg-img"></div>
-            </div>
-            <div class="right">
-                <div
-                    class="back no-drag cursor-pointer"
-                    v-if="
-                        messageData[messageKeys.History] &&
-                        messageData[messageKeys.History].length > 0
-                    "
-                    @click="back()"
-                >
-                    ←
-                <!-- TODO using icon -->
-                </div>
-                <SidePopup :shown="shownLyricsSidePopup" :position="'right'">
-                    <SongStatus />
-                </SidePopup>
-                <component :is="messageData[messageKeys.Show]" />
-                <div
-                    class="null"
-                    v-if="
-                        !messageData[messageKeys.Show] || messageData[messageKeys.Show] === 'null'
-                    "
-                >
-                    左边搜索~ (当前测试阶段)
-                </div>
-                <Audio @show-lyrics="showLyricsSidePopup" />
-            </div>
+  <div class="container" :class="platform" :style="{ '--accentColor': '#' + accentColor }">
+    <Head v-show="audioData.type === 'normal'" />
+    <div class="info" v-show="audioData.type === 'normal'">
+      <div class="left">
+        <Search></Search>
+        <Sheet></Sheet>
+        <div class="setting-but bg-img"></div>
+      </div>
+      <div class="right">
+        <div
+          class="back no-drag cursor-pointer"
+          v-if="messageData[messageKeys.History] && messageData[messageKeys.History].length > 0"
+          @click="back()"
+        >
+          ←
+          <!-- TODO using icon -->
         </div>
-        <div class="mini-info" v-if="audioData.type === 'mini'">
-            <Audio />
+        <SidePopup :shown="shownLyricsSidePopup" :position="'right'">
+          <SongStatus />
+        </SidePopup>
+        <component :is="messageData[messageKeys.Show]" />
+        <div
+          class="null"
+          v-if="!messageData[messageKeys.Show] || messageData[messageKeys.Show] === 'null'"
+        >
+          左边搜索~ (当前测试阶段)
         </div>
+        <Audio @show-lyrics="showLyricsSidePopup" />
+      </div>
     </div>
+    <div class="mini-info" v-if="audioData.type === 'mini'">
+      <Audio />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -55,94 +50,94 @@ import { windowShow } from '@/renderer/utils/window';
 import { audioData, switchAudioType } from '@/renderer/core';
 
 export default defineComponent({
-    components: {
-        Search,
-        SheetDetails,
-        SearchDetails,
-        Sheet,
-        Head,
-        Audio,
-        SidePopup,
-        SongStatus
-    },
-    name: 'Home',
-    setup() {
-        switchAudioType(audioData.type);
+  components: {
+    Search,
+    SheetDetails,
+    SearchDetails,
+    Sheet,
+    Head,
+    Audio,
+    SidePopup,
+    SongStatus
+  },
+  name: 'Home',
+  setup() {
+    switchAudioType(audioData.type);
 
-        const shownLyricsSidePopup = ref(false);
+    const shownLyricsSidePopup = ref(false);
 
-        function showLyricsSidePopup() {
-            shownLyricsSidePopup.value = !shownLyricsSidePopup.value;
-        }
-
-        function back() {
-            messageData[messageKeys.History].shift();
-            if (messageData[messageKeys.History].length > 0)
-                messageData[messageKeys.Show] = messageData[messageKeys.History][0];
-            else messageData[messageKeys.Show] = 'null';
-        }
-
-        onMounted(() => {
-            windowShow(argsData.window.id);
-        });
-
-        return {
-            platform: argsData.window.platform,
-            accentColor: argsData.window.appInfo.accentColor,
-            messageData,
-            audioData,
-            messageKeys,
-            componentShow,
-            shownLyricsSidePopup,
-            showLyricsSidePopup,
-            back
-        };
+    function showLyricsSidePopup() {
+      shownLyricsSidePopup.value = !shownLyricsSidePopup.value;
     }
+
+    function back() {
+      messageData[messageKeys.History].shift();
+      if (messageData[messageKeys.History].length > 0)
+        messageData[messageKeys.Show] = messageData[messageKeys.History][0];
+      else messageData[messageKeys.Show] = 'null';
+    }
+
+    onMounted(() => {
+      windowShow(argsData.window.id);
+    });
+
+    return {
+      platform: argsData.window.platform,
+      accentColor: argsData.window.appInfo.accentColor,
+      messageData,
+      audioData,
+      messageKeys,
+      componentShow,
+      shownLyricsSidePopup,
+      showLyricsSidePopup,
+      back
+    };
+  }
 });
 </script>
 <style lang="scss" scoped>
 @import '~@/renderer/views/scss/mixin.scss';
 
 .info {
-    height: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+
+  > .left {
     position: relative;
-    display: flex;
+    height: 100%;
+    width: 220px;
+    background-color: #e3e3e8;
+    padding: 40px 10px 0;
 
-    > .left {
-        position: relative;
-        height: 100%;
-        width: 220px;
-        background-color: #e3e3e8;
-        padding: 40px 10px 0;
+    > .setting-but {
+      @include device-pixel('~@/renderer/assets/icons/setting_btn');
+      position: absolute;
+      width: 16px;
+      height: 16px;
+      bottom: 12px;
+      cursor: pointer;
+    }
+  }
 
-        > .setting-but {
-            @include device-pixel('~@/renderer/assets/icons/setting_btn');
-            position: absolute;
-            width: 16px;
-            height: 16px;
-            bottom: 12px;
-            cursor: pointer;
-        }
+  > .right {
+    position: relative;
+    width: calc(100% - 220px);
+
+    > .back {
+      position: absolute;
+      top: 0;
+      left: 12px;
+      font: 400 22px ping-fang;
+      z-index: 1000;
     }
 
-    > .right {
-        position: relative;
-        width: calc(100% - 220px);
-
-        > .back {
-            position: absolute;
-            top: 0;
-            left: 12px;
-            font: 400 22px ping-fang;
-            z-index: 1000;
-        }
-
-        > .null {
-            padding: 40px 0;
-            text-align: center;
-            font: 400 19px ping-fang;
-        }
+    > .null {
+      padding: 40px 0;
+      text-align: center;
+      font: 400 19px ping-fang;
     }
+  }
 }
 
 .mini-info {
