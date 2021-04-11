a45f7071223c09b413fc42b73d42a8f4e12ebe5f
youliso <youliso@hotmail.com>
2021-04-11 11:13:13 +0800

    删除多余部分

--- build/script/webpack.base.config.js
+++ build/script/webpack.base.config.js
@@ -9,8 +9,28 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      dist: resolve('dist'),
+      'dist': resolve('dist'),
       '@': resolve('src')
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'ts',
+          target: 'esnext'
+          // appendTsSuffixTo: [/\.vue$/]
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
