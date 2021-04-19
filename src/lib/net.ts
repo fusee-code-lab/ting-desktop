111dfce64e0104fef0b27b5e70a1e2c93369ec54
youliso <youliso@hotmail.com>
2021-04-19 11:32:07 +0800

    update net

--- src/lib/net.ts
+++ src/lib/net.ts
@@ -1,14 +1,13 @@
 import fetch, { RequestInit, Headers } from 'node-fetch';
 import AbortController from 'node-abort-controller';
 import querystring from 'querystring';
-import { isNull } from '@/lib/index';
 
 const { appUrl } = require('@/cfg/config.json');
 
 export interface NetOpt extends RequestInit {
   authorization?: string;
+  isStringify?: boolean; //是否stringify参数（非GET请求使用）
   isHeaders?: boolean; //是否获取headers
-  isQuerystring?: boolean; //是否querystring参数
   data?: any;
   type?: NET_RESPONSE_TYPE; //返回数据类型
 }
@@ -26,12 +25,6 @@ export enum NET_RESPONSE_TYPE {
 export function AbortSignal() {
   return new AbortController();
 }
-
-/**
- * 网络请求
- * @param url string
- * @param param NetSendOpt
- * */
 
 /**
  * 错误信息包装
@@ -66,22 +59,22 @@ function fetchPromise(url: string, sendData: NetOpt): Promise<any> {
     .then(async (res) => {
       switch (sendData.type) {
         case NET_RESPONSE_TYPE.TEXT:
-          return sendData.isQuerystring ? {
+          return sendData.isHeaders ? {
             headers: await res.headers,
             data: await res.text()
           } : await res.text();
         case NET_RESPONSE_TYPE.JSON:
-          return sendData.isQuerystring ? {
+          return sendData.isHeaders ? {
             headers: await res.headers,
             data: await res.json()
           } : await res.json();
         case NET_RESPONSE_TYPE.BUFFER:
-          return sendData.isQuerystring ? {
+          return sendData.isHeaders ? {
             headers: await res.headers,
             data: await res.arrayBuffer()
           } : await res.arrayBuffer();
         case NET_RESPONSE_TYPE.BLOB:
-          return sendData.isQuerystring ? {
+          return sendData.isHeaders ? {
             headers: await res.headers,
             data: await res.blob()
           } : await res.blob();
@@ -97,8 +90,8 @@ function fetchPromise(url: string, sendData: NetOpt): Promise<any> {
 export async function net(url: string, param: NetOpt = {}): Promise<any> {
   if (url.indexOf('http://') === -1 && url.indexOf('https://') === -1) url = appUrl + url;
   let sendData: NetOpt = {
-    isQuerystring: param.isQuerystring,
     isHeaders: param.isHeaders,
+    isStringify: param.isStringify,
     headers: new Headers(Object.assign({
         'Content-type': 'application/json;charset=utf-8',
         'authorization': param.authorization || ''
@@ -109,10 +102,9 @@ export async function net(url: string, param: NetOpt = {}): Promise<any> {
     method: param.method || 'GET',
     signal: param.signal || null
   };
-  if (!isNull(param.data)) {
+  if (!!param.data) {
     if (sendData.method === 'GET') url = `${url}?${querystring.stringify(param.data)}`;
-    else if (sendData.isQuerystring) sendData.body = querystring.stringify(param.data);
-    else sendData.body = param.data;
+    else sendData.body = sendData.isStringify ? querystring.stringify(param.data) : JSON.stringify(param.data);
   }
   return Promise.race([timeoutPromise(sendData.timeout), fetchPromise(url, sendData)])
     .catch(err => errorReturn(err.message));
