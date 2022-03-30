14bbcb5dbc6b843da4f8ec6468284a42a11f8f49
youliso <youliso@Hotmail.com>
2022-03-30 14:47:37 +0800

    feat: update route

--- src/renderer/views/components/content/index.tsx
+++ src/renderer/views/components/content/index.tsx
@@ -1,27 +1,10 @@
+import { ChildRoute } from '@/renderer/common/router';
 import searchDetail from './searchDetail';
 
-type RT = 'searchDetail';
-let rI: RT = 'searchDetail';
-
-const r: { [key: string]: any } = {
-  searchDetail: new searchDetail()
-};
-
-const el = <div class="content"></div>;
-
-function removeChild() {
-  el.firstChild && el.removeChild(el.firstChild);
-}
-
-export function contentLoad() {
-  el.childNodes.length === 0 && el.appendChild(r[rI].render());
-  return el;
-}
-
-export function contentRender(path: RT) {
-  if (rI === path) return;
-  r[rI].un();
-  rI = path;
-  removeChild();
-  el.appendChild(r[rI].render());
-}
+export default new ChildRoute(
+  {
+    searchDetail: new searchDetail()
+  },
+  'searchDetail',
+  <div class="content"></div>
+);
